c749642d979cd6adc729be29635cf1d6dc98197e
rahulchavan3 <rahulchavan3030@gmail.com>
2023-01-02 13:30:57 +0530

    Created connection with mongoDb  and added dotenv file and data

---
+++ databaseConnection.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+
+function DbConnection() {
+  const DB_URL = process.env.MONGO_URI;
+
+  mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+
+  db.on("error", console.error.bind(console, "Connection error: "));
+  db.once("open", function () {
+    console.log("Db connected...");
+  });
+}
+
+module.exports = DbConnection;
+
+module.exports = DbConnection;
